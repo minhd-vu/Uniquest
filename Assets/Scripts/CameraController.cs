1f179eb8c1bab8125650db035519e7cff21ad882
Minh Vu <minhd_vu@yahoo.com>
2019-11-19 23:03:54 -0500

    added keyboard rotation support

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,14 +6,20 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private Camera cam;
     [SerializeField] private Transform target;
+    [SerializeField] private float speed;
+    [SerializeField] private float minimumZoom;
+    [SerializeField] private float maximumZoom;
+
     private Vector3 previousPosition;
+    private float currentZoom;
 
     // Update is called once per frame
     void Update()
     {
-        //Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, Input.GetAxisRaw("Vertical") * speed * Time.deltaTime
-        //transform.RotateAround(center.position, Vector3.up, Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime);
-        //transform.RotateAround(center.position, Vector3.left, Input.GetAxisRaw("Vertical") * speed * Time.deltaTime);
+        cam.transform.position = target.position;
+        cam.transform.Rotate(Vector3.right, Input.GetAxisRaw("Vertical") * speed * Time.deltaTime);
+        cam.transform.Rotate(Vector3.up, -Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, Space.World);
+        cam.transform.Translate(Vector3.back * minimumZoom);
 
         if (Input.GetMouseButtonDown(0))
         {
@@ -26,7 +32,7 @@ public class CameraController : MonoBehaviour
             cam.transform.position = target.position;
             cam.transform.Rotate(Vector3.right, direction.y * 180);
             cam.transform.Rotate(Vector3.up, -direction.x * 180, Space.World);
-            cam.transform.Translate(Vector3.back * 10);
+            cam.transform.Translate(Vector3.back * minimumZoom);
 
             previousPosition = cam.ScreenToViewportPoint(Input.mousePosition);
         }
