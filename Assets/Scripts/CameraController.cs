ea0ea8adc8c96777e33261850d0b7482587e5212
Minh Vu <minhd_vu@yahoo.com>
2019-11-23 21:44:26 -0500

    added light for player

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Camera cam;
+    [SerializeField] private Transform player;
     [SerializeField] private Transform target;
     [SerializeField] private float speed;
 
@@ -15,16 +16,23 @@ public class CameraController : MonoBehaviour
 
     private float velocity;
     private float targetFOV;
+    private bool selectedTarget;
 
     void Start()
     {
         targetFOV = cam.fieldOfView;
         velocity = 0f;
+        selectedTarget = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            selectedTarget = !selectedTarget;
+        }
+
         if (Input.GetMouseButton(1))
         {
             cam.transform.RotateAround(target.transform.position, cam.transform.up, Input.GetAxis("Mouse X") * speed);
